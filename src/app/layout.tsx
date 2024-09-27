a00fcf25a56b9669f2dc00a315201667b06459d7
chendf <1725760203@qq.com>
2024-09-27 17:14:27 +0800

    feat: init project and update eslint、config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,10 @@
 import './globals.scss'
 
 export default function RootLayout ({ children, }: Readonly<{ children: React.ReactNode }>) {
-    const {} = { a: 1, }
     return (
         <html lang="en">
-            <body>
-                {children}
-            </body>
+            <meta name="keywords" content="nextjs" />
+            <body suppressHydrationWarning={true}>{ children }</body>
         </html>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
-export default function HomPage () {
-    return <div>homPage</div>
+'use client'
+import React from 'react'
+import { useState } from 'react'
+
+const AppPage = () => {
+    const [count, setCount,] = useState(0)
+    return (
+        <div className="container">
+            {count}
+            <h1 onClick={() => {setCount(count => count + 1)}}>App Page</h1>
+        </div>
+    )
 }
+
+export default AppPage
